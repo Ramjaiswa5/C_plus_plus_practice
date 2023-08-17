a86c505c21936552ee8222a1e92b39d56f0dc072
Ram Babu Jaiswal <92715985+Ramjaiswa5@users.noreply.github.com>
2023-08-17 19:18:16 +0530

    creating a class

---
+++ OOPS/class.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+class Hero{
+    //properties
+
+    int health;
+};
+
+int main(){
+
+    Hero h1;
+
+    cout<<"size is "<<sizeof(h1);
+    return 0;
+}
