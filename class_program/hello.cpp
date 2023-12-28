fffe8ba43b30ebd76cb4045c44fe665fa0fd06df
Ram Babu Jaiswal <92715985+Ramjaiswa5@users.noreply.github.com>
2023-12-29 00:43:00 +0530

    hello_world using class

---
+++ class_program/hello.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+class sample{   
+    public:
+    void print(){
+        cout<<"Hello world";
+    }
+};
+int main(){
+    
+    sample ram;
+    
+    ram.print();
+    
+    return 0;
+}
